c25f627241c9901fa802fe22d0ad6472e8ac9f15
huynonstop <huynonstop123nt@gmail.com>
2020-02-01 13:49:34 +0700

    Updates

---
+++ precache-manifest.d535f2ec7aa2f07b32017acf9584c101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71318a86565b40d45f12ad7cf698c35",
+    "url": "/ReactTicTacToe/index.html"
+  },
+  {
+    "revision": "f47f5df684b6299107b8",
+    "url": "/ReactTicTacToe/static/css/main.cd7dab57.chunk.css"
+  },
+  {
+    "revision": "80dc369d5dcf09c7fbe2",
+    "url": "/ReactTicTacToe/static/js/2.96e2fe09.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactTicTacToe/static/js/2.96e2fe09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f47f5df684b6299107b8",
+    "url": "/ReactTicTacToe/static/js/main.e524703e.chunk.js"
+  },
+  {
+    "revision": "e24e7793b8eb574d4672",
+    "url": "/ReactTicTacToe/static/js/runtime-main.281def26.js"
+  }
+]);
